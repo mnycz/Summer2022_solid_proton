76fe32fdb66380dce3bde0893a3cb2b0c5f85189
mnycz <mnycz@jlab.org>
2022-06-21 21:26:37 -0400

    fix the fit*.sh file to remove some junk not needed

--- fits_20211017.sh
+++ fits_20211017.sh
@@ -2,31 +2,6 @@
 #Script to carry out fits of sin^2th for 10/15/2021 updated unpolarized ep/eD simulations with unfolding
 #NOTE: first argument to bash script must be "./eAll.Linux.x86_64.exe" which is the name of the fitting code executable
 
-
-$1 ep 36.8 0 true /w/halla-scshelf2102/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_ep_allq2_w4PDF_5_100.dat
+##### change the file location to where the bins file is located
+$1  /w/halla-scshelf2102/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_ep_allq2_w4PDF_5_100.dat
 wait
-$1 ep 44.8 1 true /w/halla-scshelf2102/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_ep_allq2_w4PDF_10_100.dat
-wait
-$1 ep 100.0 2 true /w/halla-scshelf2102/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_ep_allq2_w4PDF_10_275.dat
-wait
-$1 ep 15.4 3 true /w/halla-scshelf2102/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_ep_allq2_w4PDF_18_275.dat
-wait
-$1 ep 100.0 4 true /w/halla-scshelf2102/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_ep_allq2_w4PDF_18_275.dat
-wait
-
-#$1 ed 36.8 true /w/halla-scifs17exp/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_eD_allq2_w4PDF_5_41.dat
-#wait
-
-#$1 ed 36.8 0 true /w/halla-scshelf2102/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_eD_allq2_w4PDF_5_100.dat  
-#wait 
-#$1 ed 44.8 1 true /w/halla-scshelf2102/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_eD_allq2_w4PDF_10_100.dat
-#wait
-#$1 ed 100.0 2 true /w/halla-scshelf2102/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_eD_allq2_w4PDF_10_137.dat
-#wait
-#$1 ed 15.4 3 true /w/halla-scshelf2102/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_eD_allq2_w4PDF_18_137.dat
-#wait
-#$1 ed 100.0 4 true /w/halla-scshelf2102/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_eD_allq2_w4PDF_18_137.dat
-#wait
-#$1 ed 10.0 5 true /w/halla-scshelf2102/triton/mnycz/EIC/code_Alex/CT18NLO/New/asymmetry_unfolded4_eD_allq2_w4PDF_18_137.dat
-#wait
-
